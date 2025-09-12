a05ca24c406a9621deadb796b13180be84b125d5
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-12 11:31:29 -0300

    feat: create index file to export dialog components for better organization

---
+++ apps/applications/src/components/common/index.ts
@@ -0,0 +1,19 @@
+// Confirmation Dialog
+export { 
+  ConfirmationDialog, 
+  useConfirmationDialog,
+  type DialogVariant 
+} from './confirmation-dialog';
+
+// Generic Dialog
+export { 
+  GenericDialog, 
+  useGenericDialog 
+} from './generic-dialog';
+
+// Form Dialog
+export { 
+  FormDialog, 
+  useFormDialog,
+  type FormMode 
+} from './form-dialog';
