2f19766a79c7fd7ef00f3bc857470171e31af107
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-15 10:51:13 -0300

    refactor: remove organization module and related DTOs, services, and controller
    - Removed OrganizationModule, OrganizationController, and OrganizationService.
    - Deleted DTOs: AddMemberDto, CreateOrganizationDto, UpdateOrganizationDto, UpdateMemberRoleDto, OrganizationResponseDto.
    - Cleaned up app.module.ts by removing OrganizationModule import.

--- apps/api/prisma/migrations/20250915135046_first_schema/migration.sql
+++ apps/api/prisma/migrations/20250915135046_first_schema/migration.sql
@@ -1,9 +1,10 @@
 -- CreateTable
 CREATE TABLE "public"."User" (
     "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "avatarUrl" TEXT,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
