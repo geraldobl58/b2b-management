03e6c9cc54f7398ab9a28d75ef7b19a93a02c774
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-16 16:12:32 -0300

    feat(clients): refactor NewClientPage to use Box and Header components for improved layout

--- apps/applications/src/app/(admin)/clients/[new]/page.tsx
+++ apps/applications/src/app/(admin)/clients/[new]/page.tsx
@@ -1,12 +1,17 @@
+import { Box } from "@mui/material";
+import { Header } from "@/components/header";
+
 import { ClientForm } from "../components/client-form";
 
 const NewClientPage = () => {
   return (
-    <div>
-      <div>
-        <ClientForm />
-      </div>
-    </div>
+    <Box>
+      <Header
+        title="Clientes"
+        description="Gerencie os clientes da sua empresa, aqui você pode adicionar, editar ou remover clientes."
+      />
+      <ClientForm />
+    </Box>
   );
 };
 
