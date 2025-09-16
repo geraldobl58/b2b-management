cd85f183458a4e99d503cc9098502ddfc7d4decb
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-16 16:12:23 -0300

    feat(campaigns): implement Campaign component with header and button for adding campaigns

--- apps/applications/src/app/(admin)/campaigns/page.tsx
+++ apps/applications/src/app/(admin)/campaigns/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 
+import { Header } from "@/components/header";
+import { Box, Button } from "@mui/material";
+import { PlusIcon } from "lucide-react";
+
 const Campaign = () => {
   return (
-    <div>
-      <div>Campaign</div>
-    </div>
+    <Box>
+      <Header
+        title="Campanhas"
+        description="Gerencie as campanhas da sua empresa, aqui você pode adicionar, editar ou remover campanhas."
+        content={
+          <>
+            <Button className="normal-case" variant="contained" color="primary">
+              <PlusIcon className="mr-2" size={16} />
+              Adicionar Campanha
+            </Button>
+          </>
+        }
+      />
+    </Box>
   );
 };
 
