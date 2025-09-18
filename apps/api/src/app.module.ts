ff6327bce37a035cb19178f774a9cf0280022091
Geraldo Luiz <geraldobl58@gmail.com>
2025-09-18 16:05:38 -0300

    feat(app): add ContractsModule to the main application module imports

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ClientsModule } from './clients/clients.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ContractsModule } from './contracts/contracts.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { PrismaModule } from './prisma/prisma.module';
     PrismaModule,
     AuthModule,
     ClientsModule,
+    ContractsModule,
   ],
   controllers: [],
   providers: [],
